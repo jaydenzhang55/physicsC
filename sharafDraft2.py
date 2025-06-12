3f365865874f005827a34690af4e0bae760ce748
Jayden Zhang <117393725+jaydenzhang55@users.noreply.github.com>
2025-06-12 12:16:04 -0400

    Update sharafDraft2.py

--- sharafDraft2.py
+++ sharafDraft2.py
@@ -43,10 +43,10 @@ newFluidVol = fluidVol * ((273.15) / airTemperature)
 
 # variables: fluid density, velocity, drag coefficient, cross sectional area, mass, gravity, fluid volume, altitude
 
-def setMass(event):
-    global mass
-    if event.id == 'x':
-        mass = event.value
+# def setMass(event):
+#     global mass
+#     if event.id == 'x':
+#         mass = event.value
 
 #def setfluidDens():
  #   global fluidDens
@@ -56,9 +56,9 @@ def setPlanet(p):
     global homePlanet
     if p.checked:
         homePlanet = p.plan
-        setDefaults(planet)
+        setDefaults(homePlanet)
     else:
-        setDefaults(Earth)
+        setDefaults("Earth")
     for i in range(len(buttons)):
         if i != p.i:
             buttons[i].checked = False
@@ -66,7 +66,7 @@ def setPlanet(p):
 def setDefaults(x):
     global fluidDens, airTemperature, dragCoeff, planetMass, air
     if x == "Earth":
-        backgroundPic = 45
+        backgroundPic = "https://i.imgur.com/wHGxacb.png"
         fluidDens = 1.225 #at 1 bar
         airTemperature = 273.15 #Kelvin at 1 bar
         dragCoeff = 0.5 #based on Atlas rocket https://web.archive.org/web/20170313142729/http://www.braeunig.us/apollo/saturnV.htm
@@ -86,34 +86,35 @@ def setDefaults(x):
         dragCoeff = 2 #between 1.7-2.3
         planetMass = 4.8673*10**24
         air = 'Carbon Dioxide'
-    elif planet == "Mars":
+    elif x == "Mars":
         backgroundPic = "mars.png"
         fluidDens = 0.020      # kg/m³ @ surface (~0.006 bar)
         airTemperature = 210.0      # K  (–63 °C), no 1 bar level exists
         dragCoeff = 0.3
         planetMass = 6.4171e23  # kg
         air = "Carbon Dioxide"
-    elif planet == "Jupiter":
+    elif x == "Jupiter":
         backgroundPic = "jupiter.png"
         fluidDens = 0.16       # kg/m³ @ 1 bar
         airTemperature = 165.0      # K  (–108 °C)
         dragCoeff = 0.47
         planetMass = 1.8982e27  # kg
         air = "Helium"
-    elif planet == "Neptune":
+    elif x == "Neptune":
         backgroundPic = "neptune.png"
         fluidDens = 0.45       # kg/m³ @ 1 bar
         airTemperature = 72.0       # K  (–201 °C)
         dragCoeff = 0.47
         planetMass = 1.02413e26 # kg
         air = "Helium"
-    elif planet == "Uranus":
+    elif x == "Uranus":
         backgroundPic = "uranus.png"
         fluidDens = 0.22       # kg/m³ @ 1 bar
         airTemperature = 76.0       # K  (–197 °C)
         dragCoeff = 0.47
         planetMass = 8.6810e25  # kg
         air = "Helium"
+    backgroundBox.texture = backgroundPic
         
 def changeAir(evt): # stp
     global air, molarMass, fluidDens, numberOfMoles, massOfAir
@@ -179,16 +180,15 @@ def balloonSize(s):
     balloonSizeValueDisplay.text = str(sizeOfBalloonSlider.value)
     radius = sqrt(crossSectArea / pi)
     fluidVol = (4/3)*pi*radius**3
-    sizeOfBalloonMass = fluidDens * (radius ** 3)
+    sizeOfBalloonMass = fluidDens * fluidVol
     mass = sizeOfBalloonMass + payloadMass + balloonMass
-    balloon.scale(radius)
 
 balloonSizeTextDisplay = wtext(text = 'Cross Sectional Area of Balloon (m^2) = ')
 balloonSizeValueDisplay = wtext(text = str(sizeOfBalloonSlider.value))
     
 scene.append_to_caption('<div id="right">')
 scene.append_to_caption('<div style="margin-bottom: 15px;">')
-tempOfFlameSlider = slider(bind = changeTemp, min = 1, max = 700, value = 0)
+tempOfFlameSlider = slider(bind = changeTemp, min = 0, max = 350, value = 0)
 scene.append_to_caption('</div>')
 
 def changeTemp(s):
@@ -205,7 +205,7 @@ tempOfFlameValueDisplay = wtext(text = str(tempOfFlameSlider.value))
 
 scene.append_to_caption('<div id="right">')
 scene.append_to_caption('<div style="margin-bottom: 15px;">')
-windSlider = slider(bind = changeWind, min = 0, max = 1000, value = 0)
+windSlider = slider(bind = changeWind, min = -100, max = 100, value = 0)
 scene.append_to_caption('</div>')
 
 def changeWind(s):
@@ -305,7 +305,7 @@ def changeMaterial(evt):
         materialDens = 700   # Suede (approx), kg/m^3
     balloonMass = materialDens * pow(fluidVol * (3/4) / pi, (2/3)) * 4 * pi
         
-backgroundBox = box(pos = vector(0, 0, 0), size = vector(20, 20, 0.1), color = color.white, texture = "https://i.imgur.com/wHGxacb.png")
+backgroundBox = box(pos = vector(0, 0, -1), size = vector(40, 40, 0.1), texture = "https://i.imgur.com/wHGxacb.png")
 
 time = 0; dt=0.01
 
@@ -335,14 +335,26 @@ while True:
             print("height" + heightAboveSeaLvl)
             running = False
         else:
+            hotAirDensity = fluidDens * (airTemperature / (tempOfFlameSlider.value + 273.15))
+
+            newFluidVol = fluidVol * ((tempOfFlameSlider.value + 273.15) / airTemperature)    
+            sizeOfBalloonMass = newFluidVol * hotAirDensity
             mass = balloonMass + payloadMass + sizeOfBalloonMass
-            totalCrossSectionalArea = crossSectArea + crossSectAreaDueToTemp
-            balloon.radius(sqrt(totalCrossSectionalArea/pi) / 50)
+
+            newRadius = pow(((3/4) * newFluidVol / pi),(1/3))
+            totalCrossSectionalArea = pi * newRadius ** 2
+            
+            balloon.radius = (sqrt(totalCrossSectionalArea/pi) / 50)
 
             velocity = sqrt(vx**2 + vy**2)
             airPressureOut = (pressureAtSeaLevel)*(exp((-(molarMass)*gravity*heightAboveSeaLvl)/(8.314 * (airTemperature)))) #Pascals
             fluidDensOut = (airPressureOut)/((287.058)*(airTemperature)) #kg/m^3
-                                    
+
+            airPressureIn = (pressureAtSeaLevel)*(exp(-(molarMass)*gravity*heightAboveSeaLvl)/(8.314 * (tempOfFlameSlider.value + 273.15))) #Pascals
+            fluidDensIn = (airPressureIn)/((287.058)*(tempOfFlameSlider.value + 273.15)) #kg/m^3
+
+            fluidDensDiff = fluidDensOut - fluidDensIn
+                
             if velocity > 0:
                 drag_magnitude = 0.5 * fluidDensOut * velocity**2 * dragCoeff * totalCrossSectionalArea
                 dragXForce = -drag_magnitude * (vx / velocity)
@@ -352,7 +364,7 @@ while True:
                 dragYForce = 0
             
             gravForce = mass * gravity
-            buoForce = (fluidDensOut) * gravity * fluidVol  # archimedes principle
+            buoForce = (fluidDensDiff) * gravity * newFluidVol  # archimedes principle
         
             totalXForce = dragXForce
             totalYForce = buoForce - gravForce + dragYForce
