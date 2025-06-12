0600928507dec0240685cc8327759fd2b55fbd2a
Jayden Zhang <117393725+jaydenzhang55@users.noreply.github.com>
2025-06-12 11:46:41 -0400

    Update sharafDraft2.py

--- sharafDraft2.py
+++ sharafDraft2.py
@@ -39,6 +39,7 @@ massOfAir = fluidVol * fluidDens / 1000 # in g
 planetMass = 5.97219 * (10 ** 24)
 crossSectAreaDueToTemp = 0
 totalCrossSectionalArea = crossSectArea + crossSectAreaDueToTemp
+newFluidVol = fluidVol * ((273.15) / airTemperature)   
 
 # variables: fluid density, velocity, drag coefficient, cross sectional area, mass, gravity, fluid volume, altitude
 
@@ -70,43 +71,49 @@ def setDefaults(x):
         airTemperature = 273.15 #Kelvin at 1 bar
         dragCoeff = 0.5 #based on Atlas rocket https://web.archive.org/web/20170313142729/http://www.braeunig.us/apollo/saturnV.htm
         planetMass = 5.97219 * (10 ** 24)
-        air = 'air'
+        air = 'Air'
     elif x == "Saturn": #done
+        backgroundPic = "mars.png"
         fluidDens = 0.19 #at 1 bar
         airTemperature = 134 #Kelvin at 1 bar
         dragCoeff = 0.515 #based on Atlas rocket https://web.archive.org/web/20170313142729/http://www.braeunig.us/apollo/saturnV.htm
         planetMass = 5.684*10**26
-        air = 'air'
+        air = 'Helium'
     elif x == "Venus": #done
+        backgroundPic = "mars.png"
         fluidDens = 65 #kg/m^3 at surface - would be much lower at 1 bar
         airTemperature = 301.5 #Kelvin at 1 bar
         dragCoeff = 2 #between 1.7-2.3
         planetMass = 4.8673*10**24
-        air = 'air'
-    elif x == "Mars":
-        fluidDens = 00
-        airTemperature = 00
-        dragCoeff = 00
-        planetMass = 6.418*10**23
-        air = 'air'
-    elif x == "Jupiter":
-        fluidDens = 00
-        airTemperature = 00
-        dragCoeff = 0.515
-        planetMass = 5.684*10**26
-        air = 'air'
-    elif x == "Neptune":
-        fluidDens = 00
-        airTemperature = 00
-        dragCoeff = 0.515
-        planetMass = 5.684*10**26
-        air = 'air'
-    elif x == "Uranus":
-        fluidDens = 00
-        airTemperature = 00
-        dragCoeff = 0.515
-        planetMass = 5.684*10**26
-        air = 'air'
+        air = 'Carbon Dioxide'
+    elif planet == "Mars":
+        backgroundPic = "mars.png"
+        fluidDens = 0.020      # kg/m³ @ surface (~0.006 bar)
+        airTemperature = 210.0      # K  (–63 °C), no 1 bar level exists
+        dragCoeff = 0.3
+        planetMass = 6.4171e23  # kg
+        air = "Carbon Dioxide"
+    elif planet == "Jupiter":
+        backgroundPic = "jupiter.png"
+        fluidDens = 0.16       # kg/m³ @ 1 bar
+        airTemperature = 165.0      # K  (–108 °C)
+        dragCoeff = 0.47
+        planetMass = 1.8982e27  # kg
+        air = "Helium"
+    elif planet == "Neptune":
+        backgroundPic = "neptune.png"
+        fluidDens = 0.45       # kg/m³ @ 1 bar
+        airTemperature = 72.0       # K  (–201 °C)
+        dragCoeff = 0.47
+        planetMass = 1.02413e26 # kg
+        air = "Helium"
+    elif planet == "Uranus":
+        backgroundPic = "uranus.png"
+        fluidDens = 0.22       # kg/m³ @ 1 bar
+        airTemperature = 76.0       # K  (–197 °C)
+        dragCoeff = 0.47
+        planetMass = 8.6810e25  # kg
+        air = "Helium"
         
 def changeAir(evt): # stp
     global air, molarMass, fluidDens, numberOfMoles, massOfAir
@@ -129,6 +136,9 @@ def changeAir(evt): # stp
     elif air == 'Tungsten hexafluoride':
         molarMass = 297.83
         fluidDens = 13
+    elif air == 'Carbon Dioxide':
+        molarMass = 44.003
+        fluidDens = 1.98
     
     massOfAir = fluidVol * fluidDens / 1000
     numberOfMoles = massOfAir / molarMass
@@ -178,23 +188,17 @@ balloonSizeValueDisplay = wtext(text = str(sizeOfBalloonSlider.value))
     
 scene.append_to_caption('<div id="right">')
 scene.append_to_caption('<div style="margin-bottom: 15px;">')
-tempOfFlameSlider = slider(bind = changeTemp, min = 1, max = 700, value = 350)
+tempOfFlameSlider = slider(bind = changeTemp, min = 1, max = 700, value = 0)
 scene.append_to_caption('</div>')
 
 def changeTemp(s):
-    global flameTemperature, fluidVol, crossSectAreaDueToTemp
+    global flameTemperature, fluidVol, crossSectAreaDueToTemp, totalCrossSectionalArea, crossSectArea, newFluidVol
     flameTemperature = tempOfFlameSlider.value
     tempOfFlameValueDisplay.text = str(tempOfFlameSlider.value)
-    newfluidVol = fluidVol * ((tempOfFlameSlider.value + 273.15) / airTemperature)
-    incrFluidVol = newfluidVol - fluidVol
-    fluidVol = newfluidVol
-    
-    if incrFluidVol < 0:
-        radius = pow(((3/4) * incrFluidVol / pi),(1/3))
-        crossSectAreaDueToTemp = -pi * radius ** 2
-    else:
-        radius = pow(((3/4) * incrFluidVol / pi),(1/3))
-        crossSectAreaDueToTemp = pi * radius ** 2
+    newFluidVol = fluidVol * ((tempOfFlameSlider.value + 273.15) / airTemperature)    
+    newRadius = pow(((3/4) * newFluidVol / pi),(1/3))
+    totalCrossSectionalArea = pi * newRadius ** 2
+    crossSectAreaDueToTemp = totalCrossSectionalArea - crossSectArea
 
 tempOfFlameTextDisplay = wtext(text = 'Temperature of Flame (°C) = ')
 tempOfFlameValueDisplay = wtext(text = str(tempOfFlameSlider.value))
@@ -205,8 +209,9 @@ windSlider = slider(bind = changeWind, min = 0, max = 1000, value = 0)
 scene.append_to_caption('</div>')
 
 def changeWind(s):
-    global wind
+    global wind, vx
     wind = windSlider.value
+    vx = wind * 1000/3600
     windValueDisplay.text = str(windSlider.value)
 
 windTextDisplay = wtext(text = 'Wind Speed (km/h) = ')
@@ -221,7 +226,7 @@ menu(choices=choicelist, bind=changeMaterial)
 windCaption = wtext(text = ' Material: ' + material + ' ')
 scene.append_to_caption('<div></div>')
 
-airList = ['Air', 'Hydrogen', 'Nitrogen', 'Helium', 'Oxygen', 'Tungsten hexafluoride']
+airList = ['Air', 'Hydrogen', 'Nitrogen', 'Helium', 'Oxygen', 'Carbon Dioxide', 'Tungsten hexafluoride']
 
 menu(choices=airList, bind=changeAir)
 airCaption = wtext(text = ' Air: ' + air + " ")
@@ -260,6 +265,8 @@ def reset(b):
     altitudeCurve = gcurve(graph=altitudeVsTime, color=color.green)
     forceCurve = gcurve(graph=forceVsTime, color=color.blue)
     
+    scene.center = balloon.pos
+    
 startButton = button(text = "Run", pos = scene.title_anchor, bind = start)
 resetButton = button(text = "Reset", pos = scene.title_anchor, bind = reset)
 
@@ -267,24 +274,36 @@ balloon = sphere(pos = vec(0, altitude - 9, 0), radius = sqrt(totalCrossSectiona
 #attach_arrow(balloon, "velocity", color=color.green, scale=10, shaftwidth=balloon.radius/3)
 
 def changeMaterial(evt):
+    global balloonMass, fluidVol
+    materialDens = 0
     if evt.index < 1:
         balloon.texture = "https://i.imgur.com/YwqXpCA.jpeg"
+        materialDens = 1150  # Nylon, kg/m^3
     elif evt.index == 1:
         balloon.texture = "https://i.imgur.com/aHf7shx.png"
+        materialDens = 1380  # Polyester, kg/m^3
     elif evt.index == 2:
         balloon.texture = "https://i.imgur.com/z1NDKU1.png"
+        materialDens = 500   # Wicker (approx), kg/m^3
     elif evt.index == 3: 
         balloon.texture = "https://i.imgur.com/z1NDKU1.png"
+        materialDens = 8000  # Stainless steel, kg/m^3
     elif evt.index == 4: 
-        balloon.texture="https://i.imgur.com/FkrZo0R.png"
+        balloon.texture = "https://i.imgur.com/FkrZo0R.png"
+        materialDens = 8960  # Copper, kg/m^3
     elif evt.index == 5:
-        balloon.texture="https://i.imgur.com/zEuDPcK.jpeg"
+        balloon.texture = "https://i.imgur.com/zEuDPcK.jpeg"
+        materialDens = 2700  # Aluminum, kg/m^3
     elif evt.index == 6:
-        balloon.texture="https://i.imgur.com/puph7Hw.jpeg"
+        balloon.texture = "https://i.imgur.com/puph7Hw.jpeg"
+        materialDens = 1200  # Plastic (average), kg/m^3
     elif evt.index == 7:
-        balloon.texture="https://i.imgur.com/NqIjoHj.jpeg"
+        balloon.texture = "https://i.imgur.com/NqIjoHj.jpeg"
+        materialDens = 950   # Leather, kg/m^3
     elif evt.index == 8:
-        balloon.texture="https://i.imgur.com/3N4NFBM.jpeg"
+        balloon.texture = "https://i.imgur.com/3N4NFBM.jpeg"
+        materialDens = 700   # Suede (approx), kg/m^3
+    balloonMass = materialDens * pow(fluidVol * (3/4) / pi, (2/3)) * 4 * pi
         
 backgroundBox = box(pos = vector(0, 0, 0), size = vector(20, 20, 0.1), color = color.white, texture = "https://i.imgur.com/wHGxacb.png")
 
@@ -308,12 +327,18 @@ altitudeCurve = gcurve(graph=altitudeVsTime, color=color.green)
 forceCurve = gcurve(graph=forceVsTime, color=color.blue)
 
 while True:
-    global velocity, airPressure, fluidVol, heightAboveSeaLvl, flameTemperature, fluidDens, dragForce, dragXForce, dragYForce, gravForce, buoForce, totalXForce, totalYForce, ax, ay, viy, vix, vfy, vfx, posxIncr, posyIncr, finalPosX, finalPosY, altitude, vy, vx, posx, posy, time, mass
+    global velocity, airPressure, newFluidVol, fluidVol, heightAboveSeaLvl, flameTemperature, fluidDens, dragForce, dragXForce, dragYForce, gravForce, buoForce, totalXForce, totalYForce, ax, ay, viy, vix, vfy, vfx, posxIncr, posyIncr, finalPosX, finalPosY, altitude, vy, vx, posx, posy, time, mass, balloonMass, payloadMass, sizeOfBalloonMass, crossSectArea, crossSectAreaDueToTemp, totalCrossSectionalArea, wind
+    
     rate(100)
     if running:        
         if heightAboveSeaLvl < 0:
+            print("height" + heightAboveSeaLvl)
             running = False
         else:
+            mass = balloonMass + payloadMass + sizeOfBalloonMass
+            totalCrossSectionalArea = crossSectArea + crossSectAreaDueToTemp
+            balloon.radius(sqrt(totalCrossSectionalArea/pi) / 50)
+
             velocity = sqrt(vx**2 + vy**2)
             airPressureOut = (pressureAtSeaLevel)*(exp((-(molarMass)*gravity*heightAboveSeaLvl)/(8.314 * (airTemperature)))) #Pascals
             fluidDensOut = (airPressureOut)/((287.058)*(airTemperature)) #kg/m^3
@@ -334,12 +359,12 @@ while True:
             
             totalForceFinal = sqrt(totalXForce*totalXForce + totalYForce*totalYForce)
         
-#            print("buo" + buoForce)
-#            print("grav" + gravForce)
-#            print("drag" + dragYForce)
+            print("buo" + buoForce)
+            print("grav" + gravForce)
+            print("drag" + dragYForce)
         
-#            print(totalYForce)
-#            print(posy)
+            print(totalYForce)
+            print(posy)
                 
             ay = totalYForce / mass
             ax = totalXForce / mass
@@ -370,9 +395,13 @@ while True:
             vy = vfy
             vx = vfx
             
-            speedCurve.plot(time, vfy)
+            speedCurve.plot(time, abs(vfy))
             altitudeCurve.plot(time, posy+9)
             forceCurve.plot(time, totalForceFinal)
+            
+            scene.center = balloon.pos
+
+            heightAboveSeaLvl = altitude + 9
             
             time += dt
             
